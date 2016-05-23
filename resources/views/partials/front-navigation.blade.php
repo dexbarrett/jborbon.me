a46f8bc45fedf0efe1b8ca6bdaecd2f3fe131088
Judas Borbon <judas.borbon@gmail.com>
2016-05-23 17:40:19 -0500

    Fix about-me url

--- resources/views/partials/front-navigation.blade.php
+++ resources/views/partials/front-navigation.blade.php
@@ -1,4 +1,4 @@
 <ul class="nav navbar-nav navbar-right">
     <li class="active"><a href="/">Inicio</a></li>
-    <li><a href="acerca-de">Acerca de</a></li>
+    <li><a href="/acerca-de">Acerca de</a></li>
 </ul>
