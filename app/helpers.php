ca934003230aad75c73d934e6a12ea1d81d70b58
Judas Borbon <dexter.x740@gmail.com>
2017-05-22 09:57:31 -0500

    Use a simpler implementation to display book ratings without a library

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,21 @@ function formatTagsAsLabels(array $tags, $postType) {
             return '<a href="' . $url . '" class="tag-link"><span class="label label-danger post-tag"><i class="fa fa-tag"></i> ' .  $tag['name'] . '</span></a>';
     }, $tags));
 }
+
+function getBookReviewIcons($score)
+{
+    $maxScore = 5;
+    $onIconClass = 'fa fa-fw fa-thumbs-up';
+    $offIconClass = 'fa fa-fw fa-thumbs-o-up';
+
+    $onIcons = array_fill(0, $score, $onIconClass);
+    $offIcons = array_fill(0, $maxScore - $score, $offIconClass);
+
+    $icons = array_merge($onIcons, $offIcons);
+
+    $iconMarkup = array_map(function($item) {
+        return sprintf('<i class="%s"></i>', $item);
+    }, $icons);
+
+    return implode('&nbsp;', $iconMarkup);
+}
