fbf38f2280c86f2e3b40461e88b8abfc67eb2e78
Judas Borbon <dexter.x740@gmail.com>
2017-11-25 15:26:33 -0600

    Use local version of bootstrap theme

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,7 +11,7 @@
 
     @yield('custom-head')
 
-    <link href="https://bootswatch.com/sandstone/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/sandstone.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Bitter:400,700' rel='stylesheet' type='text/css'>
