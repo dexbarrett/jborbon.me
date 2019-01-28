df305a57ffc4bb7f91a14977c15ccc85f12050b7
judas.borbon <judas.borbon@gmail.com>
2019-01-28 15:28:43 -0600

    - Update Composer dependencies - Update RSS feed to include pubDate and other improvements

--- app/Services/RSS/FeedBuilder.php
+++ app/Services/RSS/FeedBuilder.php
@@ -1,6 +1,7 @@
 <?php
 namespace DexBarrett\Services\RSS;
 
+use DateTime;
 use Carbon\Carbon;
 use DexBarrett\Post;
 use Suin\RSSWriter\Feed;
@@ -40,22 +41,32 @@ class FeedBuilder {
             ->title('JBorbon blog')
             ->description('I rant and rave until I have no sense of time.')
             ->url(config('app.url'))
-            ->appendTo($rssFeed);
+            ->feedUrl(action('FeedController@render'));
 
         $publishedPosts = Post::published()
             ->ofType('post')
-            ->select(['title', 'slug', 'html_content'])
+            ->select(['title', 'slug', 'html_content', 'published_at'])
             ->latest('published_at')
             ->take(30)
             ->get();
+
+        $channel->pubDate(
+            $publishedPosts
+            ->first()
+            ->published_at->getTimestamp()
+        )->appendTo($rssFeed);
 
         foreach ($publishedPosts as $post) {
             $rssItem = new Item;
 
             $rssItem
                 ->title($post->title)
-                ->url(config('app.url') . '/' . $post->slug)
+                ->url(action('PostController@findBySlug', ['slug' => $post->slug]))
                 ->description($post->html_content)
+                ->contentEncoded($post->html_content)
+                ->pubDate($post->published_at->getTimestamp())
+                ->guid(action('PostController@findBySlug', ['slug' => $post->slug]), true)
+                ->preferCdata(true)
                 ->appendTo($channel);
         }
 
